574f47891b935ef0b4170f676978ca40c75f9ec6
doanngocphuc <doanngocphuc@gmail.com>
2026-03-10 17:05:40 +0700

    update socket server

--- server.ts
+++ server.ts
@@ -11,51 +11,51 @@ app.get("/", (req, res) => {
 
 const server = http.createServer(app)
 
-const io = new Server(server,{
-  cors:{
-    origin:"*"
+const io = new Server(server, {
+  cors: {
+    origin: "*"
   }
 })
 
-const rooms:any = {}
+const rooms: any = {}
 
-function generateRoomCode(){
-  return Math.floor(100000 + Math.random()*900000).toString()
+function generateRoomCode() {
+  return Math.floor(100000 + Math.random() * 900000).toString()
 }
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
-  console.log("Player connected:",socket.id)
+  console.log("Player connected:", socket.id)
 
-  // create room
-  socket.on("createRoom",()=>{
+  // CREATE ROOM
+  socket.on("createRoom", () => {
 
     const code = generateRoomCode()
 
     rooms[code] = {
-      players:[socket.id]
+      players: [socket.id]
     }
 
     socket.join(code)
 
-    socket.emit("roomCreated",code)
+    socket.emit("roomCreated", code)
 
-    console.log("Room created:",code)
+    console.log("Room created:", code)
 
   })
 
-  // join room
-  socket.on("joinRoom",(code)=>{
+  // JOIN ROOM
+  socket.on("joinRoom", (code) => {
 
     const room = rooms[code]
 
-    if(!room){
-      socket.emit("roomNotFound","Room not found")
+    if (!room) {
+      socket.emit("roomNotFound", "Room not found")
       return
     }
 
-    if(room.players.length >= 2){
-      socket.emit("roomFull","Room full")
+    if (room.players.length >= 2) {
+      socket.emit("roomFull", "Room full")
       return
     }
 
@@ -63,62 +63,70 @@ io.on("connection",(socket)=>{
 
     socket.join(code)
 
-    console.log("Player joined:",code)
+    console.log("Player joined:", code)
 
-    // start game
-    if(room.players.length === 2){
+    // START GAME WHEN 2 PLAYERS
+    if (room.players.length === 2) {
 
       const players = room.players
 
-      const colors:any = {}
+      const colors: any = {}
 
       colors[players[0]] = "red"
       colors[players[1]] = "black"
 
-      io.to(code).emit("gameStart",{
+      io.to(code).emit("gameStart", {
         players,
         colors
       })
 
-      console.log("Game start:",code)
+      console.log("Game start:", code)
 
     }
 
   })
 
-  // move piece
-  socket.on("move",(data)=>{
+  // MOVE PIECE
+  socket.on("move", (data) => {
 
-    const {roomCode,move} = data
+    const { roomCode, move } = data
 
-    socket.to(roomCode).emit("moveMade",move)
+    socket.to(roomCode).emit("moveMade", move)
 
   })
 
-  // leave room
-  socket.on("leaveRoom",(code)=>{
+  // LEAVE ROOM
+  socket.on("leaveRoom", (code) => {
+
+    if (!rooms[code]) return
+
+    rooms[code].players =
+      rooms[code].players.filter((id: string) => id !== socket.id)
 
     socket.leave(code)
 
-    if(rooms[code]){
-      rooms[code].players =
-        rooms[code].players.filter((id:string)=>id !== socket.id)
+    if (rooms[code].players.length === 0) {
+      delete rooms[code]
     }
 
   })
 
-  // disconnect
-  socket.on("disconnect",()=>{
+  // DISCONNECT
+  socket.on("disconnect", () => {
 
-    console.log("Player disconnected:",socket.id)
+    console.log("Player disconnected:", socket.id)
 
-    for(const code in rooms){
+    for (const code in rooms) {
 
-      if(rooms[code].players.includes(socket.id)){
+      const room = rooms[code]
+
+      if (room.players.includes(socket.id)) {
 
         io.to(code).emit("playerLeft")
 
         delete rooms[code]
+
+        console.log("Room closed:", code)
 
       }
 
@@ -130,8 +138,6 @@ io.on("connection",(socket)=>{
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT,()=>{
-
-  console.log("Server running on",PORT)
-
+server.listen(PORT, () => {
+  console.log("Server running on", PORT)
 })
